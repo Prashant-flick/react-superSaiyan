69fa2219edf7f65630c90c055779e99c71c51476
Prashant-flick <prashantkumar1251@gmail.com>
2023-10-01 22:22:08 +0530

    adding 02counter

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // let count = 15;
+
+  //using hooks in react
+  let [count, setCount] =useState(10);
+
+  const increase = () =>{
+    if(count<20){
+      count=count+1;
+      setCount(count);
+      console.log(count);
+    }else{
+      console.log(count);
+    }
+    
+  }
+  const decrease = () =>{
+    if(count>0){
+      count=count-1;
+      setCount(count);
+      console.log(count);
+    }else{
+      console.log(count);
+    }
+   
+  }
+
+  return (
+    <>
+      <h1>TRYING REACT</h1>
+      <h2>MADE BY PRASHANT KUMAR</h2>
+      <button onClick={increase}>INCREMENT COUNT={count}</button>
+      <br />
+      <button onClick={decrease}>DECREMENT COUNT={count}</button>
+    </>
+  )
+}
+
+export default App
